b88fa25ab3b719fffeff6c4e021d252134a5b069
edameow <edameow@mail.ru>
2019-08-01 03:45:39 +0300

    full

---
+++ migrations/m190731_125708_create_tag_black_list_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%tag_black_list}}`.
+ */
+class m190731_125708_create_tag_black_list_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%tag_black_list}}', [
+            'id'             => $this->primaryKey(),
+            'user_id'        => $this->integer(),
+            'black_list_tag' => $this->integer(),
+        ]);
+
+        $this->createIndex(
+            'idx-user_tag_black_list',
+            'tag_black_list',
+            'user_id'
+        );
+
+        $this->addForeignKey(
+            'fk-bl-user_tag_black_list',
+            'tag_black_list',
+            'user_id',
+            'user',
+            'id',
+            'CASCADE'
+        );
+
+        $this->createIndex(
+            'idx-tag_tag_black_list',
+            'tag_black_list',
+            'black_list_tag'
+        );
+
+        $this->addForeignKey(
+            'fk-bl-tag_tag_black_list',
+            'tag_black_list',
+            'black_list_tag',
+            'tag',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%tag_black_list}}');
+    }
+}
